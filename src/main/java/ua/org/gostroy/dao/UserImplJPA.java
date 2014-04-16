82f8c1e33c4a76aec2de6440564a1afd9382d4ed
panser <zzzibiaz>
2014-04-16 16:09:05 +0300

    add DAO JPA Implementation

---
+++ src/main/java/ua/org/gostroy/dao/UserImplJPA.java
@@ -0,0 +1,50 @@
+package ua.org.gostroy.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import ua.org.gostroy.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by panser on 4/16/14.
+ */
+//@Repository
+public class UserImplJPA implements UserDAO {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public User find(Integer id) {
+        return em.find(User.class, id);
+    }
+
+    @Override
+    public Set<User> findAll() {
+        Query query = em.createQuery("SELECT e FROM User e");
+        List users =  (List<User>) query.getResultList();
+        return new HashSet<User>(users);
+    }
+
+    @Override
+    public Integer save(User user) {
+        em.merge(user);
+        return user.getId();
+    }
+
+    @Override
+    public void update(User user) {
+        em.merge(user);
+    }
+}
