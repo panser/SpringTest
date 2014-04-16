afd97585c92466187ff05029040d0b86bd187c5c
panser <zzzibiaz>
2014-04-16 16:12:29 +0300

    add DAO JDBC Template

---
+++ src/main/java/ua/org/gostroy/dao/UserImplJDBC.java
@@ -0,0 +1,34 @@
+package ua.org.gostroy.dao;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+import ua.org.gostroy.entity.User;
+
+import java.util.Set;
+
+/**
+ * Created by panser on 4/16/14.
+ */
+//@Repository
+public class UserImplJDBC extends NamedParameterJdbcDaoSupport implements UserDAO {
+    @Override
+    public User find(Integer id) {
+//        getNamedParameterJdbcTemplate().
+        return null;
+    }
+
+    @Override
+    public Set<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Integer save(User user) {
+        return null;
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+}
