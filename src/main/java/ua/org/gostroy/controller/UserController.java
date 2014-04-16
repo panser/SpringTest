7facd21e6ae740163ff406564d41bc6f82b58fae
panser <zzzibiaz>
2014-04-16 14:34:19 +0300

    make test SpringMVC

---
+++ src/main/java/ua/org/gostroy/controller/UserController.java
@@ -0,0 +1,36 @@
+package ua.org.gostroy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.org.gostroy.entity.User;
+import ua.org.gostroy.service.UserService;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+
+/**
+ * Created by panser on 4/16/14.
+ */
+@Controller
+@Transactional
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"/testJSP/findAllResume", "/"})
+    public String testJSPFindAllResume(Model model){
+        Set<User> users = userService.findAllUser();
+        model.addAttribute("users", users);
+        return "testJSPFindAllResume";
+    }
+
+    @RequestMapping(value = {"/testJSPX/findAllResume"})
+    public String testJSPXFindAllResume(Model model){
+        Set<User> users = userService.findAllUser();
+        model.addAttribute("users", users);
+        return "testJSPXFindAllResume";
+    }
+
+}
