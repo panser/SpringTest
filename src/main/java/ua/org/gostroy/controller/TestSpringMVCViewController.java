38c83fa70fc5a75c581dda0fcc80baf373c5710d
panser <zzzibiaz>
2014-04-17 14:12:58 +0300

    add XSLT(over XsltViewResolver) SpringMVC View

--- src/main/java/ua/org/gostroy/controller/TestSpringMVCViewController.java
+++ src/main/java/ua/org/gostroy/controller/TestSpringMVCViewController.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import ua.org.gostroy.entity.User;
 import ua.org.gostroy.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
 import java.util.Set;
 
 /**
@@ -52,6 +58,23 @@ public class TestSpringMVCViewController {
         Set<User> users = userService.findAllUser();
         model.addAttribute("users", users);
         return "testFreeMakerFindAllResume";
+    }
+
+    @RequestMapping(value = {"/testXSLT/testXSLT"})
+    public ModelAndView testXSLTFindAllResume(HttpServletRequest request,
+                                        HttpServletResponse response){
+        // builds absolute path of the XML file
+        String xmlFile = "/WEB-INF/view/xsl//citizens.xml";
+        String contextPath = request.getServletContext().getRealPath("");
+        String xmlFilePath = contextPath + File.separator + xmlFile;
+
+        Source source = new StreamSource(new File(xmlFilePath));
+
+        // adds the XML source file to the model so the XsltView can detect
+        ModelAndView model = new ModelAndView("XSLTView");
+        model.addObject("testXSLT", source);
+
+        return model;
     }
 
 }
