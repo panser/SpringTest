ae2b044345ca50079f0f93d6499dbd3cd6c9f789
panser <zzzibiaz>
2014-05-07 18:24:55 +0300

    add metod-security over @RolesAllowed

--- src/main/java/ua/org/gostroy/controller/TestController.java
+++ src/main/java/ua/org/gostroy/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Map;
 
 /**
@@ -19,6 +20,7 @@ import java.util.Map;
 public class TestController {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    @RolesAllowed({"ROLE_ADMIN"})
     @RequestMapping(value = {"/testHeaders"}, method = RequestMethod.GET)
     public String testHeaders(HttpEntity<byte[]> requestEntity, Model model){
         Map<String,String> requestHeaderMap = requestEntity.getHeaders().toSingleValueMap();
